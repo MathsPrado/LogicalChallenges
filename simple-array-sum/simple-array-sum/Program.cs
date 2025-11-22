387b6893ad2f6fed9e8fe34cfecffbb1c331b59c
MathsPrado <matheusprado55@gmail.com>
2025-11-22 14:33:34 -0300

    ädd first exercice

---
+++ simple-array-sum/simple-array-sum/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace simple_array_sum;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        // Chama a função principal
+        int result = Result.simpleArraySum(ar);
+
+        Console.WriteLine(result);
+    }
+
+}
+
---
+++ simple-array-sum/simple-array-sum/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace simple_array_sum
+{
+	public class Result
+	{
+		public Result() { }
+
+        public static int simpleArraySum(List<int> ar)
+        {
+            return ar.Sum();
+        }
+    }
+}
+
