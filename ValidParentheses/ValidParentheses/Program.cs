0500ee020d7f97c1724683a96effdb560a9c7c7c
MathsPrado <matheusprado55@gmail.com>
2025-11-28 22:03:06 -0300

    exercice

---
+++ ValidParentheses/ValidParentheses/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+using ValidParentheses;
+
+string valueInput = Convert.ToString(Console.ReadLine().Trim().ToUpper());
+
+Dictionary<char, int> valid = new Dictionary<char, int>
+    {
+        { '(', 1 },
+        { '[', 2 },
+        { '{', 3 },
+    };
+
+Dictionary<char, int> notValid = new Dictionary<char, int>
+    {
+        { ')', 1 },
+        { ']', 2 },
+        { '}', 3 },
+    };
+
+if(valueInput.Length == 1)
+    return;
+
+var valueInputArray = valueInput.ToCharArray();
+
+if (!valid.ContainsKey(valueInputArray[0]))
+    return;
+
+var testeValid = Validation.IsValid(valueInput);
+
+var aaaa = Validation.Valition(valueInputArray[0], valueInputArray.Skip(1).ToList());
+
+Console.WriteLine(aaaa);
---
+++ ValidParentheses/ValidParentheses/Validation.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidParentheses
+{
+    public class Validation
+    {
+        public static bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> parenthesesMap = new Dictionary<char, char>
+            {
+                { ')', '(' },
+                { '}', '{' },
+                { ']', '[' }
+            };
+            foreach (char c in s)
+            {
+                if (parenthesesMap.Values.Contains(c))
+                {
+                    stack.Push(c);
+                }
+                else if (parenthesesMap.Keys.Contains(c))
+                {
+                    if (stack.Count == 0 || stack.Pop() != parenthesesMap[c])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return stack.Count != 0;
+        }
+
+        public static bool Valition(char currentValue, List<char> valueToCompare)
+        {
+            Dictionary<char, int> valid = new Dictionary<char, int>
+            {
+                { '(', 1 },
+                { '[', 2 },
+                { '{', 3 },
+            };
+
+            Dictionary<char, int> notValid = new Dictionary<char, int>
+            {
+                { ')', 1 },
+                { ']', 2 },
+                { '}', 3 },
+            };
+
+            if (valid.ContainsKey(valueToCompare[0]))
+            {
+                if (valueToCompare.Count == 1)
+                    return false;
+
+                if(Valition(valueToCompare[0], valueToCompare.Skip(1).ToList()))
+                    return true;
+            }
+
+            if (notValid.ContainsKey(valueToCompare[0]))
+            {
+                var value = valid[currentValue];
+                var valueCompare = notValid[valueToCompare[0]];
+                if(value == valueCompare)
+                {
+                    if(valueToCompare.Count >1)
+                        return Valition(currentValue, valueToCompare.Skip(1).ToList());
+                    
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
