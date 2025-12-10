fbaa19b50a73f44be831bd1e61f199d8027676da
MathsPrado <matheusprado55@gmail.com>
2025-12-09 21:49:26 -0300

    added 58

---
+++ LenghtLastString/LenghtLastString/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var lalal = "Matheus Prado de   lima";
+
+var result = lalal
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Last().Length;
+
+
