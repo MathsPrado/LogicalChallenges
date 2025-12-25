fe14bd6b1f26b115da1665dfacf6c3d20a5fb268
MathsPrado <matheusprado55@gmail.com>
2025-12-25 11:26:38 -0300

    added new solution

---
+++ GoodPatis/GoodPatis/GoodPairsValidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace GoodPatis
+{
+	public class GoodPairsValidation
+	{
+		public GoodPairsValidation()
+		{
+		}
+
+        public  static int NumIdenticalPairs(int[] nums)
+        {
+            int goodPairs = 0;
+            int[] frequency = new int[101];
+
+            foreach (int num in nums)
+            {
+                goodPairs += frequency[num];
+                frequency[num]++;
+            }
+
+            return goodPairs;
+        }
+    }
+}
+
---
+++ GoodPatis/GoodPatis/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GoodPatis;
+
+
+int[] nums1 = { 1, 2, 3, 1, 1, 3 };
+Console.WriteLine($"Teste 1: {GoodPairsValidation.NumIdenticalPairs(nums1)} (Esperado: 4)");
+
+// Caso de Teste 2
+int[] nums2 = { 1, 1, 1, 1 };
+Console.WriteLine($"Teste 2: {GoodPairsValidation.NumIdenticalPairs(nums2)} (Esperado: 6)");
+
+// Caso de Teste 3
+int[] nums3 = { 1, 2, 3 };
+Console.WriteLine($"Teste 3: {GoodPairsValidation.NumIdenticalPairs(nums3)} (Esperado: 0)");
+
+// Aguarda o usuário pressionar uma tecla para fechar
+Console.WriteLine("\nPressione qualquer tecla para sair...");
+Console.ReadKey();
