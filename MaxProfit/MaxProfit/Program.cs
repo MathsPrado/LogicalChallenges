6ea8669de8aade2543edae0c7c48f546080bb77a
Matheus Prado <matheusprado55@gmail.com>
2026-01-02 12:45:17 -0300

    Added 121
    Sended after 3 Wrong Answer

---
+++ MaxProfit/MaxProfit/Program.cs
@@ -0,0 +1,15 @@
+﻿using MaxProfit.Util;
+
+namespace MaxProfit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DoBestTime doBest = new DoBestTime();
+        int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
+        doBest.GetMaxProfit(prices);
+        Console.WriteLine("Hello, World!");
+    }
+}
+
---
+++ MaxProfit/MaxProfit/Util/DoBestTime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace MaxProfit.Util
+{
+	public class DoBestTime
+	{
+		public DoBestTime()
+		{
+		}
+		#region MyRegion
+		
+		public int GetMaxProfit(int[] prices)
+		{
+            //so validando se nao e null ou se e com um valor só
+            if (prices == null || prices.Length < 2) return 0;
+
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
+                }
+                // Se o preço atual menos o menor preço for maior que o lucro recorde, atualiza.
+                else if (prices[i] - minPrice > maxProfit)
+                {
+                    maxProfit = prices[i] - minPrice;
+                }
+            }
+
+            return maxProfit;
+
+        }
+		#endregion
+	}
+}
